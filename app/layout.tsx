0e9aaabce237683d7aa0c0110e91ff45e289b215
jjw <js48jjw@daum.net>
2026-01-22 16:50:33 +0900

    adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3266167319535063"
+     crossOrigin="anonymous"></script>
+
+</head>
+
       <body
         className={`${notoSansKR.variable} antialiased`}
       >
